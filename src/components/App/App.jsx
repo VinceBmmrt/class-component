3a8b259166dce59779fc3fc05a28070da13a001d
Aspitrine <loquente62@gmail.com>
2023-11-02 13:35:45 +0100

    Ajout d'un state et affichage dans le jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,27 +6,29 @@ import './App.scss';
 
 // function App => class App extends Component
 class App extends Component {
+  // Pour avoir des données d'état, state...
+  // Il faut !
+  // 1. Initialiser le state dans le constructor
+  // constructor méthode exécuter à l'instiation de la classe `new App()`
+  constructor(props) {
+    // On faisant le super(props), j'appel le constructeur de la classe parente
+    super(props);
+
+    // Puis je déclare une propriété state qui sera un objet
+    // C'est obligatoire que cette propriété s'appelle state et c'est un objet
+    this.state = {
+      counter: 0,
+    };
+  }
+
   // Au lieu de retourner directement le jsx
   // On a une méthode render qui elle va retourner le jsx
   render() {
+    // Je récupère la propriété counter de mon state
+    const { counter } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-
-          <p>
-            Edit <code>src/components/App/App.tsx</code> and save to reload.
-          </p>
-
-          <a
-            className="App-link"
-            href="https://react.dev/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header">{counter}</header>
       </div>
     );
   }
