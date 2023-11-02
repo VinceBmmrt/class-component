0ca4b0ee6d49bb921f2caa53b17a76a6f0f75ad5
Aspitrine <loquente62@gmail.com>
2023-11-02 14:26:07 +0100

    Ajout des méthodes de cycle de vie componentDidMount, componentDidUpdate et componentWillUnmount

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unused-state */
 /* eslint-disable react/prefer-stateless-function */
 import { Component } from 'react';
 import logo from '../../assets/logo.svg';
@@ -17,6 +18,7 @@ class App extends Component {
     // C'est obligatoire que cette propriété s'appelle state et c'est un objet
     this.state = {
       counter: 0,
+      otherCounter: 0,
     };
 
     // Je bind la méthode handleClickCounter à la classe App
@@ -26,8 +28,37 @@ class App extends Component {
     this.handleClickCounter = this.handleClickCounter.bind(this);
   }
 
+  // Cette méthode sera exécute au 1er rendu du composant
+  // équivalent au useEffect(() => {}, [])
+  componentDidMount() {
+    console.log(
+      'componentDidMount est exécuté au 1er rendu uniquement de mon composant'
+    );
+    // Appel API pour récupérer les données initials
+    document.title = 'React App';
+  }
+
+  // Cette méthode sera exécuté à chaque mise à jour du composant
+  // Lorsqu'une variable d'état / state est modifier
+  // Ou lorsqu'une propriété est modifier
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate est exécuté à chaque mise à jour');
+    const { counter } = this.state;
+    // Si je souhaite réagir seulement quand une propriété ou state est modifier
+    if (prevState.counter !== counter) {
+      console.log('Le counter a été modifié');
+    }
+  }
+
+  // Quand le composant va être retirer du DOM
+  componentWillUnmount() {
+    // Sert à retirer tous les effets de bord, comme les interval ou listener genre socketio pour du temps réel
+    console.log('componentWillUnmount est exécuté avant la suppression');
+  }
+
   handleClickCounter() {
     const { counter } = this.state;
+    // setState je dois lui passé un objet avec les propriétés du state que je souhaite modifier
     this.setState({ counter: counter + 1 });
   }
 
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import App from './components/App/App';
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  // React.StrictMode va afficher nos composants puis les retirer du DOM pour les remettre
+  // Cela permettra en cas d'effet de bord (interval par exemple) de mettre en avant le problème
   <React.StrictMode>
     <App />
   </React.StrictMode>
